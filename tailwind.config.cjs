460915055828ec1ce69aa2815c527f2f2c009838
Jeremy Chandler <jeremychandler94@gmail.com>
2023-02-18 17:33:10 -0600

    update tailwind config with picocss media queries

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "576px",
+        sm: "768px",
+        lg: "992px",
+        xl: "1200px",
+      },
+    },
+  },
+  plugins: [],
+};
