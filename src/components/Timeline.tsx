cc863ade6feab968ca261b11b321d837a664ccb9
Jeremy Chandler <jeremychandler94@gmail.com>
2023-02-24 08:23:45 -0600

    timeline component

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,15 +1,25 @@
 import { Timeline as MantineTimeline } from "@mantine/core";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 interface TimelineProps {
-  items: { title: string; children?: React.ReactNode }[];
+  items: { title: string; content: string }[];
 }
 
 export function Timeline({ items }: TimelineProps) {
+  const [isClientLoaded, setIsClientLoaded] = useState<boolean>(false);
+
+  useEffect(() => setIsClientLoaded(true));
+
   return (
-    <MantineTimeline active={2}>
-      {items.map(({ title, children }) => (
-        <MantineTimeline.Item title={title}>{children}</MantineTimeline.Item>
-      ))}
-    </MantineTimeline>
+    <div className={clsx(!isClientLoaded && "hidden")}>
+      <MantineTimeline active={2}>
+        {items.map(({ title, content }) => (
+          <MantineTimeline.Item key={title} title={title}>
+            {content}
+          </MantineTimeline.Item>
+        ))}
+      </MantineTimeline>
+    </div>
   );
 }
