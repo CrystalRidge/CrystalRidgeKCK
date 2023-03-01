9b214bf779286cd9117159ba16fa93d57f703687
Jeremy Chandler <jeremychandler94@gmail.com>
2023-02-28 22:01:32 -0600

    style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ export function Header() {
     </li>
   ));
   return (
-    <nav className="px-4 relative bg-white z-10">
+    <nav className="px-4 relative bg-white z-10 shadow-md">
       <ul>
         <li>
           <a href="/">
