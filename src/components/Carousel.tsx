1b247da2be92e449e7606f4eb641329488f8b03d
Jeremy Chandler <jeremychandler94@gmail.com>
2023-03-05 09:02:52 -0600

    fix css

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,5 @@
 import { Carousel as MantineCarousel } from "@mantine/carousel";
-import { Image } from "@mantine/core";
+import { Image, rem } from "@mantine/core";
 import Autoplay from "embla-carousel-autoplay";
 import { useRef } from "react";
 
@@ -11,24 +11,31 @@ export function Carousel({ imageUrls }: Carousel) {
   const autoplay = useRef(Autoplay({ delay: 2000 }));
   return (
     <MantineCarousel
-      maw={320}
+      maw={500}
       mx="auto"
       withIndicators
       styles={{
         indicator: {
-          width: "12rem",
-          height: "4rem",
+          width: rem(12),
+          height: rem(4),
           transition: "width 250ms ease",
 
           "&[data-active]": {
-            width: "rem40",
+            width: rem(40),
           },
         },
         control: {
+          width: rem(4),
           "&[data-inactive]": {
             opacity: 0,
             cursor: "default",
           },
+        },
+        slide: {
+          alignSelf: "center",
+        },
+        viewport: {
+          backgroundColor: "black",
         },
       }}
       plugins={[autoplay.current]}
