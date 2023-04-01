8232f1fee6abad672a98ed9c304a9196fb49545c
Jeremy Chandler <jeremychandler94@gmail.com>
2023-04-01 17:14:38 -0500

    add vercel

--- astro.config.js
+++ astro.config.js
@@ -6,7 +6,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [tailwind(), react()],
 });
