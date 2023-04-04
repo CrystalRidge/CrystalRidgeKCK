8fa5ffb8d906075a24a066c82564b15787b00fff
Jeremy Chandler <jeremychandler94@gmail.com>
2023-04-04 09:08:51 -0500

    wp-backup-p1

--- scripts/test.js
+++ scripts/test.js
@@ -4,6 +4,8 @@ import * as fs from "node:fs";
 
 dotenv.config({ path: ".env.local" });
 
+const outDir = "wp-backup";
+
 const config = {
   host: process.env.server,
   port: process.env.port,
@@ -11,8 +13,8 @@ const config = {
   password: process.env.password,
 };
 
-if (!fs.existsSync("./files")) {
-  fs.mkdirSync("./files");
+if (!fs.existsSync(outDir)) {
+  fs.mkdirSync(outDir);
 }
 
 var c = new Client();
@@ -20,18 +22,23 @@ c.on("ready", function () {
   c.list(function (err, list) {
     if (err) throw err;
 
+    console.log(
+      `Number of files: ${list.filter(({ type }) => type !== "d").length}`
+    );
+
     list.forEach(({ name, type }) => {
       if (name === "." || name === "..") return;
 
       // TODO recursion
       if (type === "d") return;
 
-      console.log(`Trying: ${name}`);
-
       c.get(name, function (err, stream) {
-        if (err) throw err;
-        stream.pipe(fs.createWriteStream(`files/${name}`));
-        console.log(`Downloaded: ${name}`);
+        if (err) {
+          //   console.log("ERROR:", name);
+          return;
+        }
+        stream.pipe(fs.createWriteStream(`${outDir}/${name}`));
+        // console.log("Downloaded:", name);
       });
     });
 
