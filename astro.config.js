de0c04c97e09cb45420453dc190f0d703a373f9b
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2024-05-27 19:08:55 -0500

    try this

--- astro.config.js
+++ astro.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), db()]
 });
