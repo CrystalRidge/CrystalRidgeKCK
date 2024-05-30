303d2344b76925a95ac4cdad78b0a017cd65f7e1
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2024-05-29 20:41:33 -0500

    more form fields

--- src/utils/clubhouseReservationForm.ts
+++ src/utils/clubhouseReservationForm.ts
@@ -2,22 +2,46 @@ export function getData(formData: FormData) {
   const reservationDate = new Date(formData.get("reservation-date") as string);
   const emailAddress = formData.get("email-address") as string;
   const phoneNumber = formData.get("phone-number") as string;
-  return { reservationDate, emailAddress, phoneNumber };
+  const address = formData.get("address") as string;
+  const eventType = formData.get("event-type") as string;
+  const description = formData.get("description") as string;
+  return {
+    reservationDate,
+    emailAddress,
+    phoneNumber,
+    address,
+    eventType,
+    description,
+  };
 }
 
 export function validateData({
   reservationDate,
   emailAddress,
   phoneNumber,
+  address,
+  eventType,
 }: {
   reservationDate: Date;
   emailAddress: string;
   phoneNumber: string;
+  address: string;
+  eventType: string;
+  description: string;
 }) {
-  if (!reservationDate || !emailAddress || !phoneNumber) return false;
+  if (
+    !reservationDate ||
+    !emailAddress ||
+    !phoneNumber ||
+    !address ||
+    !eventType
+  )
+    return false;
   return (
     !isNaN(reservationDate.valueOf()) &&
     typeof emailAddress === "string" &&
-    typeof phoneNumber === "string"
+    typeof phoneNumber === "string" &&
+    typeof address === "string" &&
+    typeof eventType === "string"
   );
 }
